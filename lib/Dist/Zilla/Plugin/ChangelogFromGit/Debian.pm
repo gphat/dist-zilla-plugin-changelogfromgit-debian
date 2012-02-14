55092d9088b5f3a989667d0373c057ed26153c47
Cory G Watson <gphat@onemogin.com>
2012-02-14 13:59:21 -0600

    Add allow_empty_head

--- lib/Dist/Zilla/Plugin/ChangelogFromGit/Debian.pm
+++ lib/Dist/Zilla/Plugin/ChangelogFromGit/Debian.pm
@@ -8,6 +8,19 @@ extends 'Dist::Zilla::Plugin::ChangelogFromGit';
 use DateTime::Format::Mail;
 use Text::Wrap qw(wrap fill $columns $huge);
 
+=attr allow_empty_head
+
+If true then an empty head is allowed. Since this module converts head to
+the current dzil version, this might be useful to you for some reason.
+
+=cut
+
+has 'allow_empty_head' => (
+    is => 'rw',
+    isa => 'Bool',
+    default => 1
+);
+
 =attr dist_name
 
 The distribution name for this package.
@@ -70,8 +83,11 @@ sub render_changelog {
 	
 	foreach my $release (reverse $self->all_releases) {
 
-        # Don't output empty versions.
-        next if $release->has_no_changes;
+        # Don't output empty versions, unless it's HEAD cuz that might matter!
+        next if $release->has_no_changes && $release->version ne 'HEAD';
+        if($release->version eq 'HEAD') {
+            next unless $self->allow_empty_head;
+        }
 
         my $version = $release->version;
         if($version eq 'HEAD') {
@@ -92,6 +108,12 @@ sub render_changelog {
                 $changelog .= fill("  ", "    ", '* '.$change->description)."\n";
             }
 	    }
+	    if($release->has_no_changes) {
+            $firstchange = Software::Release::Change->new(
+                description => 'no changes'
+                date => DateTime->now
+            );
+        }
         $changelog .= "\n -- ".$self->maintainer_name.' <'.$self->maintainer_email.'>  '.DateTime::Format::Mail->format_datetime($firstchange->date)."\n\n";
 	}
 	
